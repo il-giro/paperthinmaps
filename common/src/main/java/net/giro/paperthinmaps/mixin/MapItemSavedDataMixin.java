0d3ca81ade6aa97447bf007a1cba534d5fed5080
giro <riccardogir07@gmail.com>
2026-03-25 15:25:08 +0100

    removed useless comments

--- common/src/main/java/net/giro/paperthinmaps/mixin/MapItemSavedDataMixin.java
+++ common/src/main/java/net/giro/paperthinmaps/mixin/MapItemSavedDataMixin.java
@@ -5,8 +5,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.saveddata.maps.MapDecoration;
 
@@ -15,7 +13,6 @@ public class MapItemSavedDataMixin {
 
     @Inject(method = "addDecoration", at = @At("HEAD"), cancellable = true)
     private void onAddDecoration(MapDecoration.Type type, net.minecraft.world.level.LevelAccessor level, String id, double x, double z, double rotation, Component name, CallbackInfo ci) {
-        // Se il decoratore è di tipo FRAME (il marker verde delle cornici), cancelliamo l'operazione
         if (type == MapDecoration.Type.FRAME) {
             ci.cancel();
         }
